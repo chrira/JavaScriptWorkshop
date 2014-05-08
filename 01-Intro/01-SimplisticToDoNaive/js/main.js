2dfa1f652d6b25a5d8acb42c35e2f6ba98fd2772
Christoph Raaflaub <raaflaub@puzzle.ch>
2014-05-08 13:46:59 +0200

    ex. 1.1 move js from html to script file

--- 01-Intro/01-SimplisticToDoNaive/js/main.js
+++ 01-Intro/01-SimplisticToDoNaive/js/main.js
@@ -1,1 +1,13 @@
-// TODO: Separate layout and code
+window.onload = function(){
+   var addBtn = document.getElementById("addBtn");
+    addBtn.addEventListener('click', addText);
+}
+
+function addText(){
+    var input = document.getElementById("input");
+    var node=document.createElement("h3");
+    var textnode=document.createTextNode(input.value);
+    node.appendChild(textnode);
+    document.getElementById("do").appendChild(node);
+    input.value = "";
+}
