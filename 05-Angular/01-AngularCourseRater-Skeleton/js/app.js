c598a7e633972cccfbb57896817cdb26ac97066d
Christoph Raaflaub <raaflaub@puzzle.ch>
2014-05-20 13:40:34 +0200

    CourseRater impl. beginn

--- 05-Angular/01-AngularCourseRater-Skeleton/js/app.js
+++ 05-Angular/01-AngularCourseRater-Skeleton/js/app.js
@@ -1,3 +1,8 @@
 var MyCtrl = function ($scope) {
+    $scope.ratings = [];
+
+    $scope.addRating = function(){
+        $scope.ratings.push({name: $scope.name, rating: $scope.rating, entered: new Date()});
+    }
 
 };
